7c6c16fecedd2c2e9372c4da65b36794645f6afc
Yelizaveta Crofts <yscrofts@hotmail.com>
2020-11-20 17:22:19 +0100

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactMapGL from "react-map-gl";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>maaaap</p>
+    </div>
+  );
+}
+
+export default App;
