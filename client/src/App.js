1409d36a5240f56d3d84eae21a2d81686f051d11
Yelizaveta Crofts <yscrofts@hotmail.com>
2020-11-22 14:24:53 +0100

    stored API key in .env in client

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import ReactMapGL, {Marker} from "react-map-gl";
 import * as fireData from "./data/FIRE_FACILITY_WGS84.json"; //want to have this in the server instead
 import './App.css';
+import './styles/Marker.css';
 
 function App() {
   const [viewport, setViewport] = useState({
@@ -9,14 +10,15 @@ function App() {
     longitude: -79.3832,
     width: '100vw',
     height: '100vh',
-    zoom: 10
+    zoom: 10,
+    position: "center"
   })
   return (
     <div className="App">
       <p>maaaap</p>
       <ReactMapGL 
       {...viewport}
-      mapboxApiAccessToken={""}
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
       /* {process.env.REACT_APP_MAPBOX_TOKEN} <= not reading the .env .  */
       mapStyle="mapbox://styles/liskac/ckhkommsp0a8x19l9i10ihn7x"
       onViewportChange={viewport => {
@@ -25,23 +27,15 @@ function App() {
       >
 
 
-{fireData[0].map((i) => {
- // console.log(fireData)
-  return <Marker
-  key={[i]}
-  latitude={45}
-  longitude={-75}
-  >Fire</Marker>
-})}
-
-
 {fireData.default.map((i) => {
   console.log([i][0])
   return <Marker
   key={[i]}
   latitude={[i][0][1]}
   longitude={[i][0][0]}
-  >Fire</Marker>
+  className="marker"
+  offsetTop={-25}
+  />
 })}
       </ReactMapGL>
     </div>
